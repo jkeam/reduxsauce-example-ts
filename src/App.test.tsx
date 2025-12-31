791e548dd2750527ff84a568e7104f189a4732dd
Jon Keam <jpkeam@gmail.com>
2025-12-31 15:28:50 -0500

    added mock

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { render } from './test-utils';
 import App from './App';
 
+jest.mock('uuid', () => ({
+  v4: jest.fn(() => '00000000-0000-0000-0000-000000000000')
+}));
+
 test('renders title', () => {
   const { getByText } = render(<App />, {});
   const element = getByText(/Reduxsauce Typescript Example/i);
